36f95c7c9aee86bbe4ed213356d46d2a93eb79fb
abufadda01 <laithfadda4@gmail.com>
2024-01-02 08:37:07 +0300

    commit from work

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ require("dotenv").config({path : "./config/.env"})
 
 const app = express()
 
+// create 5 bootcamps routes
+// get , get with specific id
+// post
+// put with specific id
+// delete with specific id
+
+
 const PORT = process.env.PORT 
 
 app.listen(PORT , () => {
